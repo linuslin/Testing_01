67058b04d95db2313977a430382af5fdefdd742d
Linus Lin <wetas.wetah@gmail.com>
2012-05-18 15:55:19 +0800

    Try to refactoring PlayerObject form Viewcontroller

---
+++ MyTableVie3/Players.h
@@ -0,0 +1,15 @@
+//
+//  Players.h
+//  MyTableVie3
+//
+//  Created by Linus Lin on 5/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface Players : NSObject {
+    
+}
+@end
